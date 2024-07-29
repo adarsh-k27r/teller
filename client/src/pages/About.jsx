46180e6ec0a0207cce49778624bf59a963e4b6d9
Adarsh Kumar <21cs3060@rgipt.ac.in>
2024-07-29 22:50:26 +0530

    small fix

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,13 +1,12 @@
 export default function About() {
   return (
-    <div className='min-h-screen flex items-center justify-center'>
-      <div className='max-w-2xl mx-auto p-3 text-center'>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
         <div>
-          <h1 className='text-3xl font font-semibold text-center my-7'>
+          <h1 className="text-3xl font font-semibold text-center my-7">
             About Teller's
           </h1>
-          <div className='text-md text-gray-500 flex flex-col gap-6'>
-
+          <div className="text-md text-gray-500 flex flex-col gap-6">
             <p>
               We encourage you to leave comments on our posts and engage with
               other readers. You can like other people's comments and reply to
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,8 +1,11 @@
 export default function Projects() {
   return (
-    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
-      <h1 className='text-3xl font-semibold'>Projects</h1>
-      <p className='text-md text-gray-500'>Build fun and engaging projects while learning HTML, CSS, and JavaScript!</p>
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+        My other projects will be added here soon. Until then, please stay
+        tuned!
+      </p>
     </div>
-  )
+  );
 }
